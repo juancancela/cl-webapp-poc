39d7402e244da3e1b3e4913dac6aec3890870ecc
lukebell <lucas811@gmail.com>
2017-07-01 16:03:41 -0300

    Remove SuccessButton import

--- src/frontend/component/App/App.js
+++ src/frontend/component/App/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import NavigationBar from '../NavigationBar/NavigationBar.js';
-import SuccessButton from '../SuccessButton/SuccessButton.js';
 import FindYourPath from '../FindYourPath/FindYourPath'
 
 const App = () => (
